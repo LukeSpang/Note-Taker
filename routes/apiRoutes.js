f7f03f89c58a699accfeec3e1441a6dc220597ed
Luke Spang <lspang20@gmail.com>
2021-06-14 10:30:55 -0400

    api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+const { v4: uuidv } = require('uuid');
+
+module.exports = (app) => {
+    app.get("/api/notes", (req, res) => {
+        res.json(data);
+    });
+    app.get("/api/notes/:id", (req, res) => {
+        res.json(data);
+    });
+    app.post("/api/notes/:id", (req, res) => {
+        let note = req.body;
+        let uniqueID = data.length;
+        console.log(uniqueID);
+        note.id = uniqueID;
+        data.push(note);
+        req.body.id = uuidv4();
+
+        fs.writeFileSync("../db/db.json", JSON.stringify(data), (err) => {
+            if (err) console.log(err);
+        });
+        res.json(data);
+    });
+}
