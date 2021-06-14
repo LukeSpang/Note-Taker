57f0605206cfd2bcc49b39f8647bac92b0e5556c
Luke Spang <lspang20@gmail.com>
2021-06-14 09:34:34 -0400

    create and delete notes

--- server.js
+++ server.js
@@ -9,3 +9,61 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 let notes = require("./db/db.json");
+
+app.get("/notes", function (req, res){
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res){
+    fs.readFile("db/db.json", "utf8", function (err, data){
+        if (err){
+            console.log(err);
+            return;
+        }
+        res.json(notes);
+    });
+});
+
+app.listen(PORT, function(){
+    console.log(`App listening on ${PORT}`);
+});
+
+app.post("api/notes", function (req, res){
+    let randomLetter = String.fromCharCode(65 + Math.floor(Math.random()*26));
+    let id = randomLetter + Date.now();
+    let newNote = {
+        id: id,
+        title: req.body.title,
+        text:req.body.text,
+    };
+    console.log(typeof notes);
+    notes.push(newNote);
+    const stringifyNote = JSON.stringify(notes);
+    res.json(notes);
+    fs.writeFile("db/db.json", stringifyNote, (err) => {
+        if (err) console.log(err);
+        else{
+            console.log("Your note was saved to db.json");
+        }
+    });
+});
+
+app.delete("/api/notes/:id", function (req, res){
+    let noteID = req.params.id;
+    fs.readFile("db/db.json", "utf8", function (err, data){
+        let updatedNotes = JSON.parse(data).filter((note)=>{
+            console.log("note.id", note.id);
+            console.log("noteID", noteID);
+            return note.id !== noteID;
+
+        });
+        notes = updatedNotes;
+        const stringifyNote = JSON.stringify(updatedNotes);
+        fs.writeFile("db/db.json", stringifyNote, (err) =>{
+            if (err) console.log(err);
+            else{
+                console.log("Your Note was deleted from db.json");
+            }
+        })
+    })
+})
