9d3d20d1603c79957b1ff65eae2207249cdf7030
Luke Spang <lspang20@gmail.com>
2021-06-14 10:50:49 -0400

    somewhat works. on local server. save and new buttons not working

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 
-app.use(epxress.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "public"));
 
@@ -11,5 +11,5 @@ require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port: http://localhost:${PORT}`);
 }); 
